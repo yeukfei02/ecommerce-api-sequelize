a11b6d930718e8e45714149c83f95ab2acf0aed3
Donald Wu <yeukfei02@gmail.com>
2022-10-03 18:13:08 +0800

    feat: 🎸 add book api

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,81 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Book } from '../book/model/book.model';
+import { Shop } from '../shop/model/shop.model';
+import { User } from '../user/model/user.model';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+  constructor(
+    @InjectModel(Book)
+    private bookModel: typeof Book,
+  ) {}
+
+  async createBook(
+    name: string,
+    author: string,
+    price: number,
+    quantity: number,
+    shop_id: number,
+    user_id: number,
+  ): Promise<Book> {
+    const book = await this.bookModel.create({
+      name: name,
+      author: author,
+      price: price,
+      quantity: quantity,
+      shop_id: shop_id,
+      user_id: user_id,
+    });
+    return book;
+  }
+
+  async getBooks() {
+    const books = await this.bookModel.findAll({
+      include: [Shop, User],
+    });
+    return books;
+  }
+
+  async getBookById(id: number) {
+    const book = await this.bookModel.findOne({
+      where: { id: id },
+      include: [Shop, User],
+    });
+    return book;
+  }
+
+  async updateBookById(
+    id: number,
+    name: string,
+    author: string,
+    price: number,
+    quantity: number,
+    shop_id: number,
+    user_id: number,
+  ) {
+    await this.bookModel.update(
+      {
+        name: name,
+        author: author,
+        price: price,
+        quantity: quantity,
+        shop_id: shop_id,
+        user_id: user_id,
+      },
+      {
+        where: {
+          id: id,
+        },
+      },
+    );
+  }
+
+  async deleteBookById(id: number) {
+    await this.bookModel.destroy({
+      where: {
+        id: id,
+      },
+    });
+  }
+}
