847705e994de73b4b329569a192587c7a85a334d
Donald Wu <yeukfei02@gmail.com>
2022-10-03 16:12:22 +0800

    feat: 🎸 add shop migration

--- models/shop.js
+++ models/shop.js
@@ -1,27 +1,26 @@
 'use strict';
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Shop extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.hasMany(models.Shop);
-      User.hasMany(models.Book);
-      User.hasMany(models.Order);
+      Shop.hasMany(models.Book);
+      Shop.hasMany(models.Order);
     }
   }
-  User.init(
+  Shop.init(
     {
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
+      name: DataTypes.STRING,
+      address: DataTypes.STRING,
     },
     {
       sequelize,
-      modelName: 'User',
+      modelName: 'Shop',
     },
   );
-  return User;
+  return Shop;
 };
